c3884eb77ad2998a067d391822b6b71947141208
Andrea <a.masiero@dacomat.com>
2022-11-22 21:43:03 +0100

    Creation of spring boot maven project with Spring Initializr

---
+++ src/main/java/com/masy/telepasschallenge/TelepassChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.masy.telepasschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelepassChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelepassChallengeApplication.class, args);
+    }
+
+}
